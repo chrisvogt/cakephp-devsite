5d96944932fe7a5dc44c74166a1ce30d53a410ad
Chris Vogt <mail@chrisvogt.me>
2013-06-17 19:23:58 -0700

    Update schema, changing Users table (gravatar). Update cakephp ROOT directive for local install directory structure.

--- Console/cake.php
+++ Console/cake.php
@@ -16,13 +16,13 @@
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       app.Console
  * @since         CakePHP(tm) v 2.0
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 $ds = DIRECTORY_SEPARATOR;
 $dispatcher = 'Cake' . $ds . 'Console' . $ds . 'ShellDispatcher.php';
 
 if (function_exists('ini_set')) {
-	$root = dirname(dirname(dirname(__FILE__)));
+	$root = dirname(dirname(dirname(dirname(__FILE__))));
 
 	// the following line differs from its sibling
 	// /lib/Cake/Console/Templates/skel/Console/cake.php
