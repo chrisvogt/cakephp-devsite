3d598ec0ff26ea455658fd54cf8e4df59c8b1a3c
Chris Vogt <mail@chrisvogt.me>
2013-06-21 21:23:33 -0700

    Allow access to the sendEmail function.

--- Controller/ContactsController.php
+++ Controller/ContactsController.php
@@ -16,6 +16,10 @@ class ContactsController extends AppController {
      */
     public $components = array('Recaptcha.Recaptcha');
     
+    public function beforeFilter() {
+        $this->Auth->allow('sendEmail');
+    }
+    
     /**
      * Send an email using CakeEmail() to the address set in Bootstrap
      * 
@@ -24,19 +28,19 @@ class ContactsController extends AppController {
     function sendEmail() {
         if ($this->request->is('post')) {
             if ($this->Recaptcha->verify()) {
-            $this->Contact->set($this->request->data);
-            if ($this->Contact->validates()) {
-                $email = new CakeEmail();
-                $email->config('postmark');
-                
-                $email->from(Configure::read('Social.email'));
-                $email->to(Configure::read('Social.email'));
-                $email->subject('New website contact from ' . $this->data['name']);
-                
-                $email->send('From ' . $this->data['email'] . ': ' . $this->data['message']);
-                
-                $this->Session->setFlash('Your message was sent successfully.', 'flash/success');
-                $this->redirect(array('controller' => 'pages', 'action' => 'display', 'contact'));
+                $this->Contact->set($this->request->data);
+                if ($this->Contact->validates()) {
+                    $email = new CakeEmail();
+                    $email->config('postmark');
+
+                    $email->from(Configure::read('Social.email'));
+                    $email->to(Configure::read('Social.email'));
+                    $email->subject('New website contact from ' . $this->data['name']);
+
+                    $email->send('From ' . $this->data['email'] . ': ' . $this->data['message']);
+
+                    $this->Session->setFlash('Your message was sent successfully.', 'flash/success');
+                    $this->redirect(array('controller' => 'pages', 'action' => 'display', 'contact'));
                 } 
             } else {
                 $this->Session->setFlash($this->Recaptcha->error, 'flash/error');
